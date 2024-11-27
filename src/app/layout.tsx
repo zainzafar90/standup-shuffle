a794b24bde68f0dd21bf588da944cc884cbc8e1d
Zain Zafar <zainzafar90@gmail.com>
2024-11-27 19:33:32 +0500

    fix: og images and twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,43 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Standup Shuffle",
   description: "A simple tool to shuffle your standup names",
-  robots: {
-    follow: true,
-    index: true,
-  },
-  icons: {
-    icon: "/favicon.png",
-  },
   openGraph: {
     title: "Standup Shuffle",
     description: "A simple tool to shuffle your standup names",
-    images: ["/og.png"],
+    type: "website",
+    url: "https://standup-shuffle.vercel.app",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Standup Shuffle Preview",
+      },
+    ],
+    siteName: "Standup Shuffle",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Standup Shuffle",
+    description: "A simple tool to shuffle your standup names",
+    images: ["/og-image.png"],
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
